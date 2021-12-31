75565a49e030881eedbb210fe67bfccc75bed72a
a-whall <awh577868@gmail.com>
2021-12-31 15:10:25 -0500

    added new files: sound manager

---
+++ include/sound.h
@@ -0,0 +1,12 @@
+
+
+
+class Sound_Manager
+{
+public:
+    Sound_Manager();
+    ~Sound_Manager();
+
+    void play_song();
+    void play_sfx();
+};
---
+++ src/sound.cpp
@@ -0,0 +1,21 @@
+#include "sound.h"
+
+Sound_Manager::Sound_Manager()
+{
+
+}
+
+Sound_Manager::~Sound_Manager()
+{
+
+}
+
+void Sound_Manager::play_song()
+{
+
+}
+
+void Sound_Manager::play_sfx()
+{
+    
+}
