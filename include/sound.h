649250207f6319f12713f26c8a55b6fac3c5df53
a-whall <awh577868@gmail.com>
2022-01-01 00:29:13 -0500

    removed empty constructor and destructor, added load_sfx function

--- include/sound.h
+++ include/sound.h
@@ -1,12 +1,17 @@
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <unordered_map>
+#include "mapid.h"
+#include "sfxid.h"
 
-
+const char * get_audio_path(Map_ID_enum);
 
 class Sound_Manager
 {
+  Mix_Music * loaded_song = nullptr;
+  std::unordered_map<SFX_ID_enum,Mix_Chunk*> sfx_map;
 public:
-    Sound_Manager();
-    ~Sound_Manager();
-
-    void play_song();
-    void play_sfx();
+  void load_sfx();
+  void play_song(Map_ID_enum);
+  void play_sfx(SFX_ID_enum);
 };
--- src/sound.cpp
+++ src/sound.cpp
@@ -1,21 +1,43 @@
 #include "sound.h"
+#include "debug.h"
 
-Sound_Manager::Sound_Manager()
+void Sound_Manager::load_sfx()
 {
-
+  for (auto sID : {bump_wall, door_in, door_out,ledge_hop})
+  {
+    Mix_Chunk * mc = Mix_LoadWAV("assets/audio/sfx/bump_wall.wav");
+    if (mc != NULL) sfx_map[bump_wall] = mc;
+    else std::cout << "[Sound] error: " << Mix_GetError() << '\n';
+  }
 }
 
-Sound_Manager::~Sound_Manager()
+void Sound_Manager::play_song(Map_ID_enum mID)
 {
-
+  if (loaded_song != nullptr) {                  
+    Mix_FreeMusic(loaded_song);
+    loaded_song = nullptr;
+  }
+  if ( loaded_song = Mix_LoadMUS(get_audio_path(mID)) ) {
+    Mix_PlayMusic(loaded_song, -1);
+    Debug::log_from(Debug::sound,"mixer now playing ",get_audio_path(mID));
+  }
+  else
+    Debug::log_error("[Sound] ",Mix_GetError());
 }
 
-void Sound_Manager::play_song()
+void Sound_Manager::play_sfx(SFX_ID_enum sID)
 {
-
+  Mix_PlayChannel(-1, sfx_map[sID], 0);
 }
 
-void Sound_Manager::play_sfx()
+
+
+const char * get_audio_path(Map_ID_enum mID)
 {
-    
+  switch(mID)
+  {
+    case new_bark_town: return "assets/audio/music/new_bark_town.mp3";
+    case route_29:      return "assets/audio/music/route_29.mp3";
+    default:            return " due to invalid Map ID enum";
+  }
 }
