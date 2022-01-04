06d263320d95501c8a325743e7789293e909db51
a-whall <awh577868@gmail.com>
2022-01-04 11:48:12 -0500

    updated access specifiers and added counter variable for delay on key press [m] map info

--- include/map.h
+++ include/map.h
@@ -10,23 +10,22 @@ class Map
 : public Sprite
 {
   GLuint vao, vbo, ebo, current_gltexID;
-
 public:
-
   int w=32, h=32, w_tiles=0, h_tiles=0;
+  int counter=0;
   Map_ID_enum current_mID;
   World_Node * world_node;
-  bool is_visible = true;
+  bool is_visible=true;
 
   Map(Map_ID_enum mID, Camera &c, Shader &s);
   ~Map();
 
-  void load_texture(const char* mapTexPath);
-  void init_texture();
-  void init_buffers() override;
   void update(float t, const Uint8* keystates) override;
   void render() override;
   void translate(float, float);
-  bool allows_move_to(int x, int y);
   void change(Map_ID_enum mID);
+private:
+  void init_buffers() override;
+  void init_texture();
+  void try_to_load_texture();
 };
