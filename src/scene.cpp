93f2487bf7c397f43d7f09e7842db9b5e4b51d16
a-whall <awh577868@gmail.com>
2022-01-04 23:58:36 -0500

    implemented hash map for maps container

--- src/scene.cpp
+++ src/scene.cpp
@@ -7,7 +7,7 @@ namespace Scene
 {
   void Manager::init_camera(int x, int y)
   {
-    float aspect_ratio = x / (float)y;
+    float aspect_ratio (x / y);
     this->camera_controller = new Camera(glm::vec3(0, 0, 5), glm::vec3(0, 0, -1), aspect_ratio);
   }
 
@@ -15,89 +15,92 @@ namespace Scene
   {
     Camera& c = *this->camera_controller;
     sprites.push_back(new Player(*this, s));
-    sprites.back()->maps = this->maps;
     return *sprites.back();
   }
 
-  Map& Manager::init_map(Shader& s)
+  void Manager::init_maps(Shader& s)
   {
     World_Node * active_wnode = world_graph->get_current_node();
     if (active_wnode == nullptr)
       Debug::log_error_abort(Debug::scene,"manager expected the world_graph to have a current world node");
     Camera& c = *this->camera_controller;
-    maps[0] = new Map(active_wnode->mID, c, s);
-    maps[1] = new Map(null_map_id, c, s); // up
-    maps[2] = new Map(null_map_id, c, s); // left
-    maps[3] = new Map(null_map_id, c, s); // down
-    maps[4] = new Map(null_map_id, c, s); // right
-    
-    return *maps[0];
+    maps[middle] = new Map(active_wnode->mID, c, s);
+    maps[up]     = new Map(null_map_id, c, s); // up
+    maps[left]   = new Map(null_map_id, c, s); // left
+    maps[down]   = new Map(null_map_id, c, s); // down
+    maps[right]  = new Map(null_map_id, c, s); // right
   }
 
   void Manager::update_map(Map_ID_enum mID)
   {
     // This is a helper function that exchanges the map pointers to 3 of the 5 total map objects. This way, for example, when walking left onto a new map zone the maps which are already loaded on the gpu can be reused.
     // Note: I was writing this and trying to get it to work and these random translations by (0,0) worked mysteriously. I was just testing, I'm not sure why the same translation for all maps ...just works...
-    auto cascade_maps = [&](size_t i1, size_t i2) {
+    auto cascade_maps = [&](Map_Orientation i1, Map_Orientation i2) {
       Map * tmp = maps[i1];
-      maps[i1] = maps[0];
+      maps[i1] = maps[middle];
       maps[i1]->translate(0,0);
       maps[i1]->is_visible=true;
-      maps[0] = maps[i2];
-      maps[0]->translate(0,0);
-      maps[0]->is_visible=true;
+      maps[middle] = maps[i2];
+      maps[middle]->translate(0,0);
+      maps[middle]->is_visible=true;
       maps[i2] = tmp;
       maps[i2]->change(mID);
       maps[i2]->translate(0,0);
       maps[i2]->is_visible=true;
     };
     World_Node * wnode = world_graph->get_current_node();
-    if      (wnode->up() == mID) cascade_maps(3,1);
-    else if (wnode->left() == mID) cascade_maps(4,2);
-    else if (wnode->down() == mID) cascade_maps(1,3);
-    else if (wnode->right() == mID) cascade_maps(2,4);
-    maps[0]->change(mID); // this will naturally happen for non-overworld maps.
+    if      (wnode->up() == mID) cascade_maps(down,up);
+    else if (wnode->left() == mID) cascade_maps(right,left);
+    else if (wnode->down() == mID) cascade_maps(up,down);
+    else if (wnode->right() == mID) cascade_maps(left,right);
+    else maps[middle]->change(mID); // this will naturally happen for non-overworld maps.
+
     World_Node * cnode = world_graph->set_current_node(mID);
     Map_ID_enum m; // temp value for the ifs below
+    std::cout << "update maps:\n";
     if (cnode->in_overworld) {
       Debug::log_from(Debug::scene,"cnode in overworld");
-      if ((m=cnode->up()) != null_map_id) {    // if the new up map has null id according to world graph:
-        if (maps[1]->current_mID != m)            // if the up map hasn't already been swapped by cascade_maps:
-          maps[1]->change(m);                        // load the map
-        maps[1]->translate(0,maps[0]->h_tiles);   // translate the map sprite appropriately
-        maps[1]->update(0,nullptr);               // update the map object so that the correct matrix is used for rendering
-        maps[1]->is_visible= true;                // set the map to be visible
+      if ((m=cnode->up()) != null_map_id) {        // if the new up map has null id according to world graph:
+        std::cout << "maps[up].id: " << maps[up]->current_mID << " current_node.up().id: " << m << '\n';
+        if (maps[up]->current_mID != m)               // if the up map hasn't already been swapped by cascade_maps:
+          maps[up]->change(m);                           // load the map
+        maps[up]->translate(0,maps[middle]->h_tiles); // translate the map sprite appropriately
+        maps[up]->update(0,nullptr);                  // update the map object so that the correct matrix is used for rendering
+        maps[up]->is_visible= true;                   // set the map to be visible
       }
-      else maps[1]->is_visible= false;         // else turn the up maps visibility off
+      else maps[up]->is_visible= false;         // else turn the up maps visibility off
       if ((m=cnode->left()) != null_map_id) {  // repeat
-        if (maps[2]->current_mID != m)
-          maps[2]->change(m);
-        maps[2]->translate(-maps[2]->w_tiles,0);
-        maps[2]->update(0, nullptr);
-        maps[2]->is_visible= true;
+        std::cout << "maps[left].id: " << maps[left]->current_mID << " current_node.left().id: " << m << '\n';
+        if (maps[left]->current_mID != m)
+          maps[left]->change(m);
+        maps[left]->translate(-maps[left]->w_tiles,0);
+        maps[left]->update(0, nullptr);
+        maps[left]->is_visible= true;
       }
-      else maps[2]->is_visible= false;
+      else maps[left]->is_visible= false;
       if ((m=cnode->down()) != null_map_id) {
-        if (maps[3]->current_mID != m)
-          maps[3]->change(m);
-        maps[3]->translate(0,-maps[3]->h_tiles);
-        maps[3]->update(0,nullptr);
-        maps[3]->is_visible= true;
+        std::cout << "maps[down].id: " << maps[down]->current_mID << " current_node.down().id: " << m << '\n';
+        if (maps[down]->current_mID != m)
+          maps[down]->change(m);
+        maps[down]->translate(0,-maps[left]->h_tiles);
+        maps[down]->update(0,nullptr);
+        maps[down]->is_visible= true;
       }
-      else maps[3]->is_visible= false;
+      else maps[down]->is_visible= false;
       if ((m=cnode->right()) != null_map_id) {
-        if (maps[4]->current_mID != m)
-          maps[4]->change(m);
-        maps[4]->translate(maps[0]->w_tiles,0);
-        maps[4]->update(0,nullptr);
-        maps[4]->is_visible= true;
+        std::cout << "maps[right].id: " << maps[right]->current_mID << " current_node.right().id: " << m << '\n';
+        if (maps[right]->current_mID != m)
+          maps[right]->change(m);
+        maps[right]->translate(maps[middle]->w_tiles,0);
+        maps[right]->update(0,nullptr);
+        maps[right]->is_visible= true;
       }
-      else maps[4]->is_visible= false;
+      else maps[right]->is_visible= false;
     }
     else {// set neighbors invisible
       Debug::log_from(Debug::scene,"cnode is not in overworld");
-      maps[0]->change(mID);
-      maps[1]->is_visible= maps[2]->is_visible= maps[3]->is_visible= maps[4]->is_visible= false;
+      maps[middle]->change(mID);
+      maps[up]->is_visible= maps[left]->is_visible= maps[down]->is_visible= maps[right]->is_visible= false;
     }
   }
 
@@ -112,8 +115,8 @@ namespace Scene
 
   void Manager::update(float t)
   {
-    for (auto& m : maps)    m->update(t, key_states);
     for (auto& s : sprites) s->update(t, key_states);
+    for (auto& m : maps)    m.second->update(t, key_states);
   }
 
   void Manager::render()
@@ -122,10 +125,10 @@ namespace Scene
     // otherwise print only the main map (which would be a self contained indoor location)
     // after that render sprites.
     if (world_graph->get_current_node()->in_overworld) {
-      for (auto& m : maps) if (m->current_mID != null_map_id) m->render();
+      for (auto& m : maps) if (m.second->current_mID != null_map_id) m.second->render();
     }
     else {
-      maps[0]->render();
+      maps[middle]->render();
     }
     for (auto& s : sprites) s->render();
     // TODO: render grass, etc, things that need to be rendered over the player
@@ -134,7 +137,7 @@ namespace Scene
   void Manager::clean()
   {
     for (auto& s: sprites) delete s;
-    for (auto& m: maps)    delete m;
+    for (auto& m: maps)    delete m.second;
     for (auto& s: shaders) delete s;
   }
 
