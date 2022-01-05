92e90bcea59fd559fc80e8e399717c5dd9a3e3e4
a-whall <awh577868@gmail.com>
2022-01-04 23:41:55 -0500

    removed player and map pointer

--- src/game.cpp
+++ src/game.cpp
@@ -2,8 +2,8 @@
 
 namespace Game
 {
-  Map    * p_active_map= nullptr;
-  Player * p_player= nullptr;
+  //Map    * p_active_map= nullptr;
+  //Player * p_player= nullptr;
   Shader * map_shader= nullptr;
   Shader * player_shader= nullptr;
 
@@ -15,8 +15,8 @@ namespace Game
     player_shader= &scene_manager.new_shader("shader/PlayerSprite.glsl");
     scene_manager.world_graph= world= new World_Graph();;
     world->set_current_node(player_house_fl1);
-    p_active_map = &scene_manager.init_map(*map_shader);
-    p_player = &scene_manager.new_player(*player_shader);
+    scene_manager.init_maps(*map_shader);
+    scene_manager.new_player(*player_shader);
   }
 
   void Instance::on_update(float t) { /* Empty function */ }
