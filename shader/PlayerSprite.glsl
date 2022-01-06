9e140b08675ac52e136384467652711a0506ca20
a-whall <awh577868@gmail.com>
2022-01-05 23:44:16 -0500

    added comments and changed variable names

--- shader/PlayerSprite.glsl
+++ shader/PlayerSprite.glsl
@@ -1,33 +1,36 @@
 #shader vertex
 #version 460
 
-layout (location = 0) in vec3 VertexPosition;
-layout (location = 1) in vec2 VertexTexCoord;
+// read from vertex buffer, location 0 -> attrib pointer 0
+layout (location = 0) in vec3 vertex_position;
+layout (location = 1) in vec2 vertex_tex_coord;
 
-out vec3 Position;
-out vec2 TexCoord;
+// sent to subsequent fragment shader invocations
+out vec3 position;
+out vec2 tex_coord;
 
-uniform mat4 ProjectionMatrix;
-uniform mat4 MVP;
+// uniforms shared by all vertices
+uniform mat4 mvp;
 
-void main() {
-    TexCoord = VertexTexCoord;
-    gl_Position = MVP * vec4(VertexPosition, 1.0);
+void main()
+{
+  tex_coord = vertex_tex_coord;                   // out tex_coor 
+  gl_Position = mvp * vec4(vertex_position, 1.0); // the clip-space output position of the current vertex
 }
 
 #shader fragment
 #version 460
 
-in vec3 Position;
-in vec2 TexCoord;
+in vec3 position;
+in vec2 tex_coord;
 
-layout( location = 0 ) out vec4 FragColor;
-layout( binding = 0 ) uniform sampler2DArray PlayerTex; // The texture sampler object, requires #version 420
+layout( location = 0 ) out vec4 frag_color;
+layout( binding = 0 ) uniform sampler2DArray player_tex; // The texture sampler object, requires #version 420
 
-uniform int frameID;
+uniform int frame_ID;
 
 void main()
 {
-  FragColor = texture(PlayerTex, vec3(TexCoord, frameID));
+  frag_color = texture(player_tex, vec3(tex_coord, frame_ID));
 }
 #end
