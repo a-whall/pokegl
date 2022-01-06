ef3f4105468358615a2655810f24ee102ec00c27
a-whall <awh577868@gmail.com>
2022-01-05 23:50:01 -0500

    added text shader init call and moved play song to after shader compilation

--- src/game.cpp
+++ src/game.cpp
@@ -4,17 +4,20 @@ namespace Game
 {
   Shader * map_shader= nullptr;
   Shader * player_shader= nullptr;
+  Shader * text_shader= nullptr;
 
   void Instance::on_init()
   {
     scene_manager.sound.load_sfx();
-    scene_manager.sound.play_song(new_bark_town);
     map_shader= &scene_manager.new_shader("shader/MapSprite.glsl");
     player_shader= &scene_manager.new_shader("shader/PlayerSprite.glsl");
+    text_shader= &scene_manager.new_shader("shader/Text.glsl");
+    scene_manager.init_text(*text_shader);
     scene_manager.world_graph= world= new World_Graph();;
     world->set_current_node(player_house_fl1);
     scene_manager.init_maps(*map_shader);
     scene_manager.new_player(*player_shader);
+    scene_manager.sound.play_song(new_bark_town);
   }
 
   void Instance::on_update(float t) { /* Empty function */ }
