04c4c596932d3a1ccb69300d5d4709afb094d711
a-whall <awh577868@gmail.com>
2022-01-07 19:13:45 -0500

    added identify() implementation

--- src/sprite.cpp
+++ src/sprite.cpp
@@ -1,4 +1,5 @@
 #include "sprite.h"
+#include "debug.h"
 
 Sprite::Sprite(float x, float y, float z, Camera &c, Shader &s)
 : cam(c), shader(s)
@@ -12,5 +13,6 @@ Sprite::~Sprite()
   // empty
 }
 
+void Sprite::identify() { Debug::log_from(Debug::sprite,std::hex,this,std::dec); };
 mat4 Sprite::get_model()       { return model; };
 void Sprite::set_model(mat4 m) { model = m; };
