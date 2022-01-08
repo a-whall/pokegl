f7cfd544cf299fb834a2d4ffe5accca971e6fccd
a-whall <awh577868@gmail.com>
2022-01-07 19:07:53 -0500

    added new member textbox sprite and modified init_text to accept the shader for that

--- include/scene.h
+++ include/scene.h
@@ -9,6 +9,7 @@
 #include "sound.h"
 #include "map_orientation.h"
 #include "text.h"
+#include "textbox.h"
 
 namespace Scene
 {
@@ -21,16 +22,18 @@ namespace Scene
     vector<Shader*> shaders;
   public:
     Text_Sprite * text= nullptr;
+    Text_Box_Sprite * textbox= nullptr;
     unordered_map<Map_Orientation,Map*> maps;
     Sound_Manager sound;
     World_Graph * world_graph= nullptr;
     Camera * camera_controller= nullptr;
     const Uint8 * key_states= nullptr;
+    void print_objects();
     void init_camera(int, int);
     Player& new_player(Shader & s);
     Shader& new_shader(const char *);
     void init_maps(Shader & s);
-    void init_text(Shader & s);
+    void init_text(Shader & s, Shader& s2);
     void update_map(Map_ID_enum);
     void update(float t);
     void render();
