517a544ff0308d0c7bb3babe657ae7df5a05451d
a-whall <awh577868@gmail.com>
2022-01-07 19:13:04 -0500

    added textbox changes

--- src/game.cpp
+++ src/game.cpp
@@ -5,6 +5,7 @@ namespace Game
   Shader * map_shader= nullptr;
   Shader * player_shader= nullptr;
   Shader * text_shader= nullptr;
+  Shader * text_box_shader= nullptr;
 
   void Instance::on_init()
   {
@@ -12,7 +13,8 @@ namespace Game
     map_shader= &scene_manager.new_shader("shader/MapSprite.glsl");
     player_shader= &scene_manager.new_shader("shader/PlayerSprite.glsl");
     text_shader= &scene_manager.new_shader("shader/Text.glsl");
-    scene_manager.init_text(*text_shader);
+    text_box_shader= &scene_manager.new_shader("shader/TextBox.glsl");
+    scene_manager.init_text(*text_shader, *text_box_shader);
     scene_manager.world_graph= world= new World_Graph();;
     world->set_current_node(player_house_fl1);
     scene_manager.init_maps(*map_shader);
