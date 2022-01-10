013a27a9f32027a7d1b7ca5ac681b7249b6623a7
a-whall <awh577868@gmail.com>
2022-01-09 21:30:01 -0500

    added song struct for sound manager abstraction, added soundid.h

--- include/sound.h
+++ include/sound.h
@@ -2,16 +2,24 @@
 #include <SDL_mixer.h>
 #include <unordered_map>
 #include "mapid.h"
-#include "sfxid.h"
+#include "soundid.h"
 
-const char * get_audio_path(Map_ID_enum);
+const char * get_audio_path(Track_ID);
+const char * get_sfx_path(SFX_ID);
+Track_ID get_area_track(Map_ID);
+
+struct Song
+{
+  Track_ID tid = null_track;
+  Mix_Music * ptr = nullptr;
+};
 
 class Sound_Manager
 {
-  Mix_Music * loaded_song = nullptr;
+  Song loaded_song;
   std::unordered_map<SFX_ID_enum,Mix_Chunk*> sfx_map;
 public:
   void load_sfx();
-  void play_song(Map_ID_enum);
+  void play_song(Track_ID);
   void play_sfx(SFX_ID_enum);
 };
