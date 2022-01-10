c269147529b14789229295c5f6e4eb602146f53d
a-whall <awh577868@gmail.com>
2022-01-10 18:13:12 -0500

    new texture management system using load textures function

---
+++ src/texture.cpp
@@ -0,0 +1,59 @@
+#include "texture.h"
+using std::vector;
+
+#include <SDL_image.h>
+using ::IMG_Load;
+
+#include <cstring>
+using std::memcpy;
+
+#include "mapid.h"
+using ::Map_ID_enum;
+
+#include "debug.h"
+using namespace Debug;
+
+
+
+// This is a function that will load a list of given texture files using the SDL image function IMG_Load. 
+// It loads the pixel data into a dynamically allocated array and returns a pointer to that array. 
+// Caller must give a list of textures that : A ) all exist according to their path, 
+// and B ) are of equal dimmensions with respect to width and height. 
+// The caller is responsible for calling delete on the pointer to pixel data that is returned from this function. 
+// Generally if a texture is not found, it's an easy fix 
+// either don't send that bad path to this function or make it a good path, 
+// so if it gets a bad path the program will crash and give a very informative message so that the bug can be fixed quickly.
+// @param path_list: a reference to a std vector filled with c strings to be passed to IMG_Load in sequence
+// @param s: the pokegl class that is using this function, TODO: test if this can work with Map_ID::none
+// @param sender: the address of the pokegl class instance that is calling this function (only used for printing on an error)
+// @param set_width,set_height: use these parameters to assign width and height somewhere else. (only samples the first texture loaded)
+// @param BPP: Bytes Per Pixel
+GLubyte *load_textures(Path_List path_list, Debug_Source_enum s, void *sender, int *set_width, int *set_height, GLsizei BPP)
+{
+  SDL_Surface *loaded_image= nullptr;
+  GLubyte *p_pixel_data= nullptr;
+  GLsizei BPF, offset= 0;
+  bool first= true;
+
+  for (auto& path : path_list)
+  {
+    loaded_image= IMG_Load(path);
+
+    if (loaded_image == nullptr)
+      log_error_abort(s,obj_addr(sender),": texture-load failed\n\t ",
+        red,path,reset,"\n\t ",IMG_GetError(),'\n');
+
+    if (first) {
+      first=false;
+      if (set_width != nullptr) *set_width= loaded_image->w;
+      if (set_height != nullptr) *set_height= loaded_image->h;
+      BPF = BPP * loaded_image->w * loaded_image->h;
+      p_pixel_data= new GLubyte[BPF * path_list.size()];
+    }
+
+    memcpy(p_pixel_data + offset, loaded_image->pixels, BPF);
+    SDL_FreeSurface(loaded_image);
+    offset += BPF;
+  }
+  return p_pixel_data;
+}
