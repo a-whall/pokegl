e1c43020b4e8c78367c5edede16ee5c59f4f1688
a-whall <awh577868@gmail.com>
2022-01-11 14:04:11 -0500

    changes to implement PokeGL class

--- src/game.cpp
+++ src/game.cpp
@@ -1,31 +1,29 @@
 #include "game.h"
 
-namespace Game
+
+
+void PokeGL::on_init()
 {
-  Shader * map_shader= nullptr;
-  Shader * player_shader= nullptr;
-  Shader * text_shader= nullptr;
-  Shader * text_box_shader= nullptr;
+  scene_manager.sound.load_sfx();
+  Shader * player_shader= &scene_manager.new_shader("shader/PlayerSprite.glsl");
+  scene_manager.init_text();
+  scene_manager.world_graph= world= new World_Graph();;
+  world->set_current_node(player_house_fl1);
+  scene_manager.init_maps();
+  scene_manager.new_player(*player_shader);
+  scene_manager.sound.play_song(new_bark_town_mp3);
+}
 
-  void Instance::on_init()
-  {
-    scene_manager.sound.load_sfx();
-    map_shader= &scene_manager.new_shader("shader/MapSprite.glsl");
-    player_shader= &scene_manager.new_shader("shader/PlayerSprite.glsl");
-    text_shader= &scene_manager.new_shader("shader/Text.glsl");
-    text_box_shader= &scene_manager.new_shader("shader/TextBox.glsl");
-    scene_manager.init_text(*text_shader, *text_box_shader);
-    scene_manager.world_graph= world= new World_Graph();;
-    world->set_current_node(player_house_fl1);
-    scene_manager.init_maps(*map_shader);
-    scene_manager.new_player(*player_shader);
-    scene_manager.sound.play_song(new_bark_town_mp3);
-  }
 
-  void Instance::on_update(float t) { /* Empty function */ }
 
-  void Instance::on_exit()
-  {
-    delete world;
-  }
+void PokeGL::on_update(float t)
+{
+  /* Empty function */
+}
+
+
+
+void PokeGL::on_exit()
+{
+  delete world;
 }
