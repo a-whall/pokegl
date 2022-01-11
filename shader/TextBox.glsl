0f255661e7fa7ee07b08a26e746fa1e95ff174d6
a-whall <awh577868@gmail.com>
2022-01-11 14:18:05 -0500

    updated vertex buffer to hold vec2 position rather than unnecessary vec3. also added a uniform mat2 which can be used to keep vertex data constant and adjust it to where the text box should appear. right now the matrix scales the textbox sprite by 2 in the x and by .8 in the y. the vertex position then gets shifted down by a constant .6, this transformation replicates the original position read from buffer prior to these changes

--- shader/TextBox.glsl
+++ shader/TextBox.glsl
@@ -1,21 +1,22 @@
 #shader vertex
-#version 460
+#version 440
 
-layout (location = 0) in vec3 vertex_position;
+layout (location = 0) in vec2 vertex_position;
 layout (location = 1) in vec2 vertex_texcoord;
 
 out vec2 tex_coord;
 
-uniform mat4 transform_to_menu_position;
+uniform mat2 adj= mat2(2, 0,  0, .8);
 
 void main()
 {
-  tex_coord = vertex_texcoord;
-  gl_Position = vec4(vertex_position, 1.0);
+  tex_coord= vertex_texcoord;
+  vec2 adj_pos= adj * vertex_position;
+  adj_pos -= vec2(0, .6);
+  gl_Position= vec4(adj_pos, 0, 1);
 }
-
 #shader fragment
-#version 460
+#version 440
 
 in vec2 tex_coord;
 
@@ -25,6 +26,6 @@ layout( binding = 0 ) uniform sampler2D textbox_texture;
 
 void main()
 {
-  textbox_frag_color = texture(textbox_texture,tex_coord);
+  textbox_frag_color = texture( textbox_texture, tex_coord );
 }
 #end
