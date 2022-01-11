ab27206291173e0eaabd87c63070d8e167702cf0
a-whall <awh577868@gmail.com>
2022-01-11 14:03:12 -0500

    removed Game namespace, and renamed ambiguous class name 'Instance' to 'PokeGL' in the global namespace

--- include/game.h
+++ include/game.h
@@ -6,18 +6,12 @@
 #include "warps.h"
 #include "world.h"
 
-namespace Game
+class PokeGL
+: public Application
 {
-  extern Player* p_player;
-  extern Map* p_active_map;
-
-  class Instance
-  : public Application
-  {
-    World_Graph * world;
-  public:
-    void on_init() override;
-    void on_update(float t) override;
-    void on_exit() override;
-  };
-}
+  World_Graph * world;
+public:
+  void on_init() override;
+  void on_update(float t) override;
+  void on_exit() override;
+};
