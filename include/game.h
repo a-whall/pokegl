7aee76f6bf38cdb2fc9be4f7d5c2fba74cc77afc
a-whall <awh577868@gmail.com>
2022-01-11 20:35:12 -0500

    removed world pointer ownership

--- include/game.h
+++ include/game.h
@@ -9,9 +9,8 @@
 class PokeGL
 : public Application
 {
-  World_Graph * world;
 public:
   void on_init() override;
-  void on_update(float t) override;
+  void on_update(float) override;
   void on_exit() override;
 };
--- src/game.cpp
+++ src/game.cpp
@@ -7,8 +7,7 @@ void PokeGL::on_init()
   scene_manager.sound.load_sfx();
   Shader * player_shader= &scene_manager.new_shader("shader/PlayerSprite.glsl");
   scene_manager.init_text();
-  scene_manager.world_graph= world= new World_Graph();;
-  world->set_current_node(player_house_fl1);
+ (scene_manager.world_graph=new World_Graph())->set_current_node(player_house_fl1);
   scene_manager.init_maps();
   scene_manager.new_player(*player_shader);
   scene_manager.sound.play_song(new_bark_town_mp3);
@@ -18,12 +17,12 @@ void PokeGL::on_init()
 
 void PokeGL::on_update(float t)
 {
-  /* Empty function */
+// empty
 }
 
 
 
 void PokeGL::on_exit()
 {
-  delete world;
+// empty
 }
