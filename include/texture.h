22f4cfec73b3dd7ad95cc3d8ba7b119c2b696295
a-whall <awh577868@gmail.com>
2022-01-11 20:40:06 -0500

    added fn: generate_texture( target, handle )

--- include/texture.h
+++ include/texture.h
@@ -5,4 +5,6 @@
 
 typedef std::vector<const char *> Path_List;
 
+void generate_texture(GLenum, GLuint*);
+
 GLubyte * load_textures(Path_List path_list, Debug::Debug_Source_enum src, void *sender, int *set_width, int *set_height, GLsizei BPP);
