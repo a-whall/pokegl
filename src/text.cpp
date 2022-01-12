291fd595958ebf00981eec5dcf5dab40fd6d3d78
a-whall <awh577868@gmail.com>
2022-01-11 20:45:30 -0500

    added fn: update(), render(). updated 'text' to 'textlines'

--- src/text.cpp
+++ src/text.cpp
@@ -2,15 +2,35 @@
 #include "scene.h"
 
 
+
 Text_Manager::Text_Manager(Scene::Manager &man)
 : scene_manager(man)
 {
-  text= new Text_Sprite(man, man.new_shader("shader/Text.glsl"));
   textbox = new Text_Box_Sprite(man, man.new_shader("shader/TextBox.glsl"));
+  textlines= new Text_Sprite(man, man.new_shader("shader/Text.glsl"));
 }
+
+
 
 Text_Manager::~Text_Manager()
 {
-  delete text;
+  delete textlines;
   delete textbox;
+}
+
+
+// Text Manager may be seen as the state of text rendering in PokeGL. 
+// This update function will update both scene objects textbox and textlines, in that order.
+void Text_Manager::update(float t, const Uint8 * key_states)
+{
+  textbox->update(t, key_states);
+  textlines->update(t, key_states);
+}
+
+
+
+void Text_Manager::render()
+{
+  textbox->render();
+  textlines->render();
 }
