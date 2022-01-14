140d1c14f9859e72e0a93b1ea6950b3635f7e185
a-whall <awh577868@gmail.com>
2022-01-13 21:59:12 -0500

    made the Shader function overloads return references instead of pointers

--- include/shader.h
+++ include/shader.h
@@ -23,14 +23,14 @@ public:
 
   void log_program_resources();
 
-  Shader* set(const char* name, int i);
-  Shader* set(const char* name, bool b);
-  Shader* set(const char* name, float f);
-  Shader* set(const char* name, float x, float y, float z);
-  Shader* set(const char* name, float x, float y, float z, float w);
-  Shader* set(const char* name, glm::vec3 v);
-  Shader* set(const char* name, glm::vec4 v);
-  Shader* set(const char* name, const glm::mat2 m);
-  Shader* set(const char* name, const glm::mat3& m);
-  Shader* set(const char* name, const glm::mat4& matrix);
+  Shader& set(const char* name, int i);
+  Shader& set(const char* name, bool b);
+  Shader& set(const char* name, float f);
+  Shader& set(const char* name, float x, float y, float z);
+  Shader& set(const char* name, float x, float y, float z, float w);
+  Shader& set(const char* name, glm::vec3 v);
+  Shader& set(const char* name, glm::vec4 v);
+  Shader& set(const char* name, const glm::mat2 m);
+  Shader& set(const char* name, const glm::mat3& m);
+  Shader& set(const char* name, const glm::mat4& matrix);
 };
