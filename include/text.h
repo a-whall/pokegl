dc0e1fc62ff5df0f10a389e9209c19e2e446a8f0
a-whall <awh577868@gmail.com>
2022-01-14 18:54:38 -0500

    added state variables from former TextLines class and also the method update_gl_char_buffer

--- include/text.h
+++ include/text.h
@@ -1,13 +1,14 @@
 #pragma once
-#include "textlines.h"
-#include "textbox.h"
+#include <string>    // std::string
+#include <SDL.h>     // Uint8
+#include <GL/glew.h> // GLuint
 
 
-// The philosophy I am choosing is to have all text be rendered and updated by a single object. 
-// This is the purpose of Text_Manager. To encapsulate everything to do with text.
+// All text will be rendered and updated by a single object: Text_Manager.
 
 namespace Scene { class Manager; }
 
+class Shader;
 
 //  state {
 //    dialog,
@@ -22,19 +23,22 @@ typedef std::string Text_Buffer;
 
 class Text_Manager
 {
-  Scene::Manager &scene_manager; // reference to the owner of this Text_Manager
-  Text_Buffer text_buffer; // data
+  Scene::Manager &scene_manager;             // reference to the owner of this Text_Manager
+  Text_Buffer text_buffer;                   // string data
+  Shader &text_box, &text_lines;             // 2 shaders
+  GLuint va, val, vbb, vbl, eb, ssb, tb, tl; // opengl object handles
+  int * char_buffer;                         // opengl string buffer
 
 public:
 
+  int eos_index;    // the size of the data string
+  int text_index=0; // render this many instances of the text character shader.
   bool has_text=false;
-
-  Text_Lines *textlines;
-  Text_Box *textbox;
 
   Text_Manager(Scene::Manager &man);
   ~Text_Manager();
 
+  void update_gl_char_buffer(std::string&);
   void set_text(std::string&&);
   void update(float, const Uint8 *);
   void render();
