1d5fbc92a192c19983b9e33864928d10aac551b5
a-whall <awh577868@gmail.com>
2022-01-29 12:54:57 -0500

    new file: texid.h which can be 1 to 1 with actual textures which gives better definition to the mapid as a world location, which is not 1 to 1 with map textures

---
+++ include/texid.h
@@ -0,0 +1,157 @@
+#pragma once
+
+enum Tex_ID : unsigned
+{
+  // generic
+  pokecenter_tex,
+  pokemart_tex,
+  house_1_tex,
+  // New Bark Town
+  new_bark_town_tex,
+  player_house_fl1_tex,
+  player_house_fl2_tex,
+  elms_lab_tex,
+  elms_house_tex,
+  // Route 29
+  route_29_tex,
+  // Cherry Grove City
+  cherry_grove_city_tex,
+  // Route 30
+  route_30_tex,
+//   // Route 31
+//   route_31_tex,
+//   // Violet City
+//   violet_city_tex,
+//   violet_gym_tex,
+//   sprout_tower_tex,
+//   violet_school_tex,
+//   violet_house_1_tex,
+//   violet_house_2_tex,
+//   // Route 32
+//   route_32_tex,
+//   // Ruins of Alph
+//   ruins_of_alph_tex,
+//   // Union Cave
+//   union_cave_tex,
+//   // Route 33
+//   route_33_tex,
+//   // Azalea Town
+//   azalea_town_tex,
+//   azalea_gym_tex,
+//   charcoal_kiln_tex,
+//   slowpoke_well_tex,
+//   kurts_house_tex,
+//   azalea_mart_tex,
+//   azalea_pc_tex,
+//   // Ilex Forest
+//   ilex_forest_tex,
+//   // Route 34
+//   route_34_tex,
+//   daycare_tex,
+//   // Goldenrod City
+//   goldenrod_city_tex,
+//   goldenrod_dept_store_tex,
+//   goldenrod_game_corner_tex,
+//   goldenrod_radio_tower_tex,
+//   goldenrod_tunnel_tex,
+//   goldenrod_pc_tex,
+//   // Route 35
+//   route_35_tex,
+//   // Route 36
+//   route_36_tex,
+//   // Route 37
+//   route_37_tex,
+//   // Ecruteak City
+//   ecruteak_city_tex,
+//   ecruteak_bell_tower_tex,
+//   ecruteak_burned_tower_tex,
+//   ecruteak_dance_theatre_tex,
+//   ecruteak_gym_tex,
+//   ecruteak_mart_tex,
+//   ecruteak_pc_tex,
+//   ecruteak_house_1_tex,
+//   // Route 38
+//   route_38_tex,
+//   // Farm
+//   farm_tex,
+//   // Route 39
+//   route_39_tex,
+//   // Olivine City
+//   olivine_city_tex,
+//   olivine_gym_tex,
+//   olivine_lighthouse_tex,
+//   olivine_house_1_tex,
+//   olivine_house_2_tex,
+//   olivine_house_3_tex,
+//   olivine_house_4_tex,
+//   olivine_ss_aqua_tex,
+//   olivine_mart_tex,
+//   olivine_pc_tex,
+//   // Route 40
+//   route_40_tex,
+//   // Route 41
+//   route_41_tex,
+//   // Cianwood City
+//   cianwood_city_tex,
+//   cianwood_gym_tex,
+//   cianwood_pc_tex,
+//   cianwood_house_1_tex,
+//   cianwood_house_2_tex,
+//   cianwood_house_3_tex,
+//   cianwood_house_4_tex,
+//   cianwood_pharmacy_tex,
+//   // Route 42
+//   route_42_tex,
+//   // Mahogany Town
+//   mahogany_town_tex,
+//   mahogany_gym_tex,
+//   mahogany_mart_tex,
+//   mahogany_pc_tex,
+//   mahogany_house_tex,
+//   // Route 43
+//   route_43_tex,
+//   // Lake of Rage
+//   lake_of_rage_tex,
+//   // Route 44
+//   route_44_tex,
+//   // Blackthorn City
+//   blackthorn_city_tex,
+//   blackthorn_gym_tex,
+//   blackthorn_mart_tex,
+//   blackthorn_pc_tex,
+//   blackthorn_house_1_tex,
+//   blackthorn_house_2_tex,
+//   blackthorn_house_3_tex,
+//   // Route 45
+//   route_45_tex,
+//   // Route 46
+//   route_46_tex,
+//   // Route 27
+//   route_27_tex,
+  max_tex_id
+};
+
+typedef  Tex_ID  Texture_ID_enum;
+typedef  Tex_ID  Tex_ID_enum;
+
+
+
+constexpr const char * tex_str[max_tex_id]=
+{
+  // generic
+  "pokecenter_tex",
+  "pokemart_tex",
+  "house_1_tex",
+  // New Bark Town
+  "new_bark_town_tex",
+  "player_house_fl1_tex",
+  "player_house_fl2_tex",
+  "elms_lab_tex",
+  "elms_house_tex",
+  // Route 29
+  "route_29_tex",
+  // Cherry Grove City
+  "cherry_grove_city_tex",
+  // Route 30
+  "route_30_tex"
+};
