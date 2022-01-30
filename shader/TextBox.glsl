2e461f0dd6e9e4949487e29d80908da0c3c54e62
a-whall <awh577868@gmail.com>
2022-01-30 17:40:19 -0500

    updated buffer attribute location since it now corresponds to one of the scene managers buffers

--- shader/TextBox.glsl
+++ shader/TextBox.glsl
@@ -1,28 +1,36 @@
 #shader vertex
 #version 440
 
-layout (location = 1) in vec4 vertex_buffer_data; // location 1 points to vec4( pos:vec2 , tex_coord:vec2 )
+layout (location = 0)
+in vec4 vbuf_data;
 
 out vec2 tex_coord;
 
 uniform mat2 scale = mat2(2, 0, 0, .8);
 uniform vec2 shift = vec2(0, -.6);
 
+
 void main()
 {
-  vec2 position = vertex_buffer_data.xy;
-  tex_coord = vertex_buffer_data.zw;
+  vec2 position = vbuf_data.xy;
+  tex_coord = vbuf_data.zw;
   vec2 adj = scale * position + shift;
   gl_Position = vec4(adj.xy, 0, 1);
 }
+
+
+
 #shader fragment
 #version 440
 
 in vec2 tex_coord;
 
-layout( location = 0 ) out vec4 textbox_frag_color;
+layout (location= 0)
+out vec4 textbox_frag_color;
 
-layout( binding = 0 ) uniform sampler2D textbox_texture;
+layout (binding= 0)
+uniform sampler2D textbox_texture;
+
 
 void main()
 {
