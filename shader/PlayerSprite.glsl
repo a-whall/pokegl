01484c46b8ea0500922360fb67220401e35a26c9
a-whall <awh577868@gmail.com>
2022-01-30 17:39:15 -0500

    updated vertex input data to now be a single attribute, packing tex coords and 2d position into a vec4

--- shader/PlayerSprite.glsl
+++ shader/PlayerSprite.glsl
@@ -1,28 +1,34 @@
 #shader vertex
 #version 460
 
-out vec2 tex_coord;
+layout (location = 0)
+in vec4 vbuf_data;
 
-layout (location = 0) in vec3 vertex_position;
-layout (location = 1) in vec2 vertex_tex_coord;
+out vec2 tex_coord;
 
 uniform mat4 player_mvp;
 
 void main()
 {
-  tex_coord = vertex_tex_coord;
-  gl_Position = player_mvp * vec4(vertex_position, 1.0); // clip-space output position of the current vertex
+  vec2 pos = vbuf_data.xy;
+  tex_coord = vbuf_data.zw;
+  gl_Position = player_mvp * vec4(pos, 0, 1); // NDC output position of the current vertex
 }
+
+
+
 #shader fragment
 #version 460
 
 in vec2 tex_coord;
 
-layout( location = 0 ) out vec4 player_frag_color;
+layout (location = 0)
+out vec4 player_frag_color;
 
-layout( binding = 0 ) uniform sampler2DArray player_tex; // texture sampler object, requires #version 420
+layout (binding = 0)
+uniform sampler2DArray player_tex; // texture sampler object, requires #version 420
 
-uniform int player_frame_ID;
+uniform int player_frame_ID = 2; // 2 corresponds to the frame idle_d
 
 void main()
 {
